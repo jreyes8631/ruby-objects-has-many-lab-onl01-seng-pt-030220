7cad95e4871c6bb1f188538ea1133a7b7c18cc42
Jose E Reyes <jreyes8631@gmail.com>
2020-04-01 02:08:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,10 @@
-class Song 
-  attr_accessor :name, :artist 
-
-  @@all = []
-
-  def initialize(name, artist=nil)
-    @name = name
-    @artist = artist
-    @@all << self
+class Song
+  attr_accessor :title
+ 
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
   end
-
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-  def self.all
-    @@all
-  end
+ 
 end
